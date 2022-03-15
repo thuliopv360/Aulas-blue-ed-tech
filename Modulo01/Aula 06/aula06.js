e63983dba6517bc73299696b483b2ad9505edb15
Thulio Paiva <thuliopv360@gmail.com>
2022-03-14 21:43:56 -0300

    ok

--- Modulo01/Aula 06/aula06.js
+++ Modulo01/Aula 06/aula06.js
@@ -1,7 +1,7 @@
 console.clear()
 const prompt = require(`prompt-sync`)();
 
-
+/*
 let aluno = "Gabriel";
 let aluno01 = "Fernando";
 let aluno02 = "Tiago";
@@ -9,4 +9,10 @@ let professor = "Gabriel pereira";
 let professora = ["Duda", "Londrina"]
 let alunos = ["Gabriel", "Fernando", "Tiago", professor, ["C13", "Blue"], professora]
 
-console.log(alunos);
+console.log(alunos);*/
+
+const filmes = ["Jurassic Park", "O Senhor dos Aneis", "Harry Potter", [1993, 2002, 2001]];
+
+console.log(filmes);
+console.log(filmes.length);
+console.log(filmes[3][2]);
