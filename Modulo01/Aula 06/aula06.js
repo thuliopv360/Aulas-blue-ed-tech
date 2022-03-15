0f9430877521f85cb6fd798143e08470123f08fb
Thulio Paiva <thuliopv360@gmail.com>
2022-03-14 21:27:37 -0300

    terceiro

---
+++ Modulo01/Aula 06/aula06.js
@@ -0,0 +1,12 @@
+console.clear()
+const prompt = require(`prompt-sync`)();
+
+
+let aluno = "Gabriel";
+let aluno01 = "Fernando";
+let aluno02 = "Tiago";
+let professor = "Gabriel pereira";
+let professora = ["Duda", "Londrina"]
+let alunos = ["Gabriel", "Fernando", "Tiago", professor, ["C13", "Blue"], professora]
+
+console.log(alunos);
