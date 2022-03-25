cebd138bf3140e946305e629825e9ef3527c180a
Thulio Paiva <thuliopv360@gmail.com>
2022-03-24 21:47:40 -0300

    exemplo

---
+++ Modulo01/Aula 11/exemplo.js
@@ -0,0 +1,8 @@
+let nome = "Thulio. Dinailton. Alexandre";
+
+let listasplit = nome.split(`l`, 2);
+
+console.log(listasplit);
+console.log(listasplit[0]);
+console.log(listasplit[1]);
+console.log(listasplit[2]);
